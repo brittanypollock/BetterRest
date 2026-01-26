edf743fbc910e493cf0a6705171fdb52505b6c04
Brittany Pollock <brittpollock15@gmail.com>
2026-01-26 15:25:46 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Brittany Pollock on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
